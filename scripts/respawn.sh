6fef9264a4ea06786173e671f28d1401c866df2b
AlexGDC <alexandre.guyon@apsys-airbus.com>
2023-09-05 23:12:49 +0200

    scroll problem fixed

--- scripts/respawn.sh
+++ scripts/respawn.sh
@@ -5,8 +5,16 @@ cd ~/thebluesharvest
 sudo chown -R www-data:www-data /srv
 sudo chmod -R a+w /srv
 
+
+cd ./frontdoor/media/
+cp -R * /srv/thebluesharvest_media/
+cd ../..
+
 pip install -r requirements_demo.txt
 
 python ./manage.py migrate
 python ./manage.py collectstatic --no-input
+
+python ./manage.py shell < ./scripts/prepare_prod.py
+
 ./scripts/super.sh restart thebluesharvest
